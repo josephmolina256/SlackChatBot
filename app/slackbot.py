d506a0d2113fbe77ecda2dd81aa10b22c7987b68
Joseph Molina <josephmolina256@gmail.com>
2025-03-05 16:38:53 -0500

    fixed formatting on link

--- app/slackbot.py
+++ app/slackbot.py
@@ -58,7 +58,7 @@ def handle_message(event, say):
                 res = (
                         f"{client.chat(context).wait_until_done()}\n\n"
                         f"Response time: {time() - now:.2f} seconds\n"
-                        f"References:\n{"\n".join([f"{i+1}. https://{SLACK_WORKSPACE_NAME}.slack.com/archives/{item['properties']['channel_id']}/p{item['properties']['thread_ts'].replace('.', '')}?thread_ts={item['properties']['thread_ts'].replace('.', '')}&cid={item['properties']['channel_id']}" for i, item in enumerate(retrieved_data)])}\n"
+                        f"References:\n{"\n".join([f"{i+1}. https://{SLACK_WORKSPACE_NAME}.slack.com/archives/{item['properties']['channel_id']}/p{item['properties']['thread_ts'].replace('.', '')}?thread_ts={item['properties']['thread_ts']}&cid={item['properties']['channel_id']}" for i, item in enumerate(retrieved_data)])}\n"
                 )
             else:
                 res = (
